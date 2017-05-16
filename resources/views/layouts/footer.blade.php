1499b85d0012c9e6ae0cd248f5ec077276b811e8
liudong <liudong@xmmakeit.com>
2017-05-16 10:19:05 +0800

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
                 <div class="col-sm-12">
                     <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }}</a></span> |
                     <span>闽ICP备17011213号</span> |
-                    <span>Powered by <a href="https://github.com/lufficc/Xblog">Xblog</a> </span>|
+                    <span>Powered by <a href="{{ config('social.github.url') }}">Xblog</a> </span>|
                     <a href="{{ url('feed.xml') }}" rel="feed" type="application/rss+xml"
                        title="Feed"><img width="13"
                                          src="{{ asset('image/icon/icon1.png') }}"
